610f838bd4b6274cc3805ef40c8ef54084363719
Jack Gough <goughy000@gmail.com>
2018-02-22 20:46:47 +0000

    Split out MXBean and it's registration

--- src/main/java/com/testingsyndicate/hc/jmx/HcJmx.java
+++ src/main/java/com/testingsyndicate/hc/jmx/HcJmx.java
@@ -2,24 +2,22 @@ package com.testingsyndicate.hc.jmx;
 
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 
-import javax.management.*;
+import javax.management.JMException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 import java.lang.management.ManagementFactory;
 import java.util.Hashtable;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class HcJmx
-    extends StandardMBean implements PoolStatsMXBean {
+public class HcJmx {
 
-    private static final AtomicInteger COUNT = new AtomicInteger();
-    private static final MBeanServer SERVER = ManagementFactory.getPlatformMBeanServer();
     private static final String JMX_DOMAIN = "org.apache.httpcomponents.httpclient";
     private static final String DEFAULT_NAME = "default-%s";
+    private static final AtomicInteger COUNT = new AtomicInteger();
+    private static final MBeanServer SERVER = ManagementFactory.getPlatformMBeanServer();
 
-    private final PoolingHttpClientConnectionManager connectionManager;
-
-    HcJmx(PoolingHttpClientConnectionManager connectionManager) {
-        super(PoolStatsMXBean.class, true);
-        this.connectionManager = connectionManager;
+    private HcJmx() {
     }
 
     public static ObjectName register(PoolingHttpClientConnectionManager connectionManager) throws JMException {
@@ -28,52 +26,17 @@ public class HcJmx
     }
 
     public static ObjectName register(PoolingHttpClientConnectionManager connectionManager, String name) throws JMException {
-        HcJmx bean = new HcJmx(connectionManager);
+        PoolingHttpClientConnectionManagerMXBean bean = new PoolingHttpClientConnectionManagerMXBean(connectionManager);
 
         ObjectName jmxName = getObjectName(name);
         SERVER.registerMBean(bean, jmxName);
         return jmxName;
     }
 
-    public static void unregister(ObjectName name) throws JMException {
+    public static synchronized void unregister(ObjectName name) throws JMException {
         if (SERVER.isRegistered(name)) {
             SERVER.unregisterMBean(name);
         }
-    }
-
-    @Override
-    public int getMaxTotal() {
-        return connectionManager.getMaxTotal();
-    }
-
-    @Override
-    public int getDefaultMaxPerRoute() {
-        return connectionManager.getDefaultMaxPerRoute();
-    }
-
-    @Override
-    public int getLeased() {
-        return connectionManager.getTotalStats().getLeased();
-    }
-
-    @Override
-    public int getPending() {
-        return connectionManager.getTotalStats().getPending();
-    }
-
-    @Override
-    public int getAvailable() {
-        return connectionManager.getTotalStats().getAvailable();
-    }
-
-    @Override
-    public int getMax() {
-        return connectionManager.getTotalStats().getMax();
-    }
-
-    @Override
-    public int getRoutesTotal() {
-        return connectionManager.getRoutes().size();
     }
 
     private static ObjectName getObjectName(String name) throws MalformedObjectNameException {
@@ -83,4 +46,5 @@ public class HcJmx
 
         return ObjectName.getInstance(JMX_DOMAIN, properties);
     }
+
 }
---
+++ src/test/java/com/testingsyndicate/hc/jmx/PoolingHttpClientConnectionManagerMXBeanTest.java
@@ -0,0 +1,128 @@
+package com.testingsyndicate.hc.jmx;
+
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.pool.PoolStats;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.management.MalformedObjectNameException;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+public class PoolingHttpClientConnectionManagerMXBeanTest {
+
+    private PoolingHttpClientConnectionManager mockConnectionManager;
+    private PoolStats mockPoolStats;
+    private PoolingHttpClientConnectionManagerMXBean sut;
+
+    @Before
+    public void setUp() throws MalformedObjectNameException {
+
+        mockPoolStats = mock(PoolStats.class);
+        mockConnectionManager = mock(PoolingHttpClientConnectionManager.class);
+
+        given(mockConnectionManager.getTotalStats()).willReturn(mockPoolStats);
+
+        sut = new PoolingHttpClientConnectionManagerMXBean(mockConnectionManager);
+    }
+
+    @Test
+    public void returnsMaxTotal() {
+        // given
+        given(mockConnectionManager.getMaxTotal()).willReturn(99);
+
+        // when
+        int actual = sut.getMaxTotal();
+
+        // then
+        then(mockConnectionManager).should().getMaxTotal();
+        assertThat(actual).isEqualTo(99);
+    }
+
+    @Test
+    public void returnsDefaultMaxPerRoute() {
+        // given
+        given(mockConnectionManager.getDefaultMaxPerRoute()).willReturn(100);
+
+        // when
+        int actual = sut.getDefaultMaxPerRoute();
+
+        // then
+        then(mockConnectionManager).should().getDefaultMaxPerRoute();
+        assertThat(actual).isEqualTo(100);
+    }
+
+    @Test
+    public void returnsLeased() {
+        // given
+        given(mockPoolStats.getLeased()).willReturn(101);
+
+        // when
+        int actual = sut.getLeased();
+
+        // then
+        then(mockPoolStats).should().getLeased();
+        assertThat(actual).isEqualTo(101);
+    }
+
+    @Test
+    public void returnsPending() {
+        // given
+        given(mockPoolStats.getPending()).willReturn(102);
+
+        // when
+        int actual = sut.getPending();
+
+        // then
+        then(mockPoolStats).should().getPending();
+        assertThat(actual).isEqualTo(102);
+    }
+
+    @Test
+    public void returnsAvailable() {
+        // given
+        given(mockPoolStats.getAvailable()).willReturn(103);
+
+        // when
+        int actual = sut.getAvailable();
+
+        // then
+        then(mockPoolStats).should().getAvailable();
+        assertThat(actual).isEqualTo(103);
+    }
+
+    @Test
+    public void returnsMax() {
+        // given
+        given(mockPoolStats.getMax()).willReturn(104);
+
+        // when
+        int actual = sut.getMax();
+
+        // then
+        then(mockPoolStats).should().getMax();
+        assertThat(actual).isEqualTo(104);
+    }
+
+    @Test
+    public void returnsRoutesTotal() {
+        // given
+        Set<HttpRoute> mockRoutes = mock(Set.class);
+        given(mockRoutes.size()).willReturn(3);
+        given(mockConnectionManager.getRoutes()).willReturn(mockRoutes);
+
+        // when
+        int actual = sut.getRoutesTotal();
+
+        // then
+        then(mockConnectionManager).should().getRoutes();
+        then(mockRoutes).should().size();
+        assertThat(actual).isEqualTo(3);
+    }
+
+}
