1285c4702fadb0df70fbe1a00f55029cf4fb45f2
Jack Gough <goughy000@gmail.com>
2018-09-15 10:38:26 +0100

    Add JavaDoc for HcJmx

--- src/main/java/com/testingsyndicate/hc/jmx/HcJmx.java
+++ src/main/java/com/testingsyndicate/hc/jmx/HcJmx.java
@@ -19,11 +19,26 @@ public final class HcJmx {
   private HcJmx() {
   }
 
+  /**
+   * Registers a {@link PoolingHttpClientConnectionManager} with JMX, using a default generated name
+   *
+   * @param connectionManager {@link PoolingHttpClientConnectionManager} to be registered with JMX
+   * @return {@link ObjectName} pointer to the registered instance
+   * @throws JMException thrown if the {@link PoolingHttpClientConnectionManager} cannot be registered
+   */
   public static ObjectName register(PoolingHttpClientConnectionManager connectionManager) throws JMException {
     String name = String.format(DEFAULT_NAME, UUID.randomUUID());
     return register(connectionManager, name);
   }
 
+  /**
+   * Registers a {@link PoolingHttpClientConnectionManager} with JMX, using a supplied name
+   *
+   * @param connectionManager {@link PoolingHttpClientConnectionManager} to be registered with JMX
+   * @param name name to be used when registering the MXBean with JMX
+   * @return {@link ObjectName} pointer to the registered instance
+   * @throws JMException thrown if the {@link PoolingHttpClientConnectionManager} cannot be registered
+   */
   public static ObjectName register(PoolingHttpClientConnectionManager connectionManager, String name) throws JMException {
     PoolingHttpClientConnectionManagerMXBean bean = new PoolingHttpClientConnectionManagerMXBean(connectionManager);
 
@@ -32,6 +47,12 @@ public final class HcJmx {
     return jmxName;
   }
 
+  /**
+   * Unregisters an MXBean from JMX
+   *
+   * @param name {@link ObjectName} to unregister
+   * @throws JMException if the MXBean cannot be unregistered
+   */
   public static synchronized void unregister(ObjectName name) throws JMException {
     if (SERVER.isRegistered(name)) {
       SERVER.unregisterMBean(name);
